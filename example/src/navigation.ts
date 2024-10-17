7ee23244160888ee8bd26123b7f0c83c88af30e1
Nadim Radjab <nadim.radjab@wolkenwerk.cloud>
2024-10-17 11:57:57 +0200

    package sub

--- example/src/navigation.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-export type ParamList = {
-  'QuickSQLite Example': undefined;
-  'Unit Tests': undefined;
-  Benchmarks: undefined;
-};
